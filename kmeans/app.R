cc0b1eb5e420e57f4635eec7944674721c18550d
Peiwen Zhang <peiwen.zhang1994@gmail.com>
2018-04-30 11:30:59 -0400

    change

--- kmeans/app.R
+++ kmeans/app.R
@@ -6,36 +6,39 @@ library(readr)
 library(shiny)
 
 # Load data
-obama <- read_csv("~/Desktop/Projects/Stat041_p4_grp3/App/obama.csv")
+obama <- read_csv("obama.csv")
 obama_kmeans <- obama %>%
   select(track_name, energy, valence)
 
-trudeau <- read_csv("~/Desktop/Projects/Stat041_p4_grp3/App/trudeau.csv")
+trudeau <- read_csv("trudeau.csv")
 trudeau_kmeans <- trudeau %>%
   select(track_name, energy, valence)
 
 ### UI Side ####
 
-# Use fluid page layout (rows and columns)
 ui <- fluidPage(
   
-  titlePanel("How many clusters to create?"),
+  titlePanel("K-Means Clustering for Obama's and Trudeau's Songs: How Many Clusters to Create?"),
   
   sidebarLayout(
     
     sidebarPanel(
-      helpText("Spotify Data"),
+      helpText("Data from Spotify"),
       
+      # Allow user to choose k
       sliderInput(inputId = "kval", 
-                  label = "Choose a value for k: ",
-                  min = 2, max = 8, value = 5)),
+                  label = "Choose a value for k, the number of clusters: ",
+                  min = 2, max = 8, value = 3)),
+    
+    p("This shiny app is made for the final project of Stat041. Complete code and datasets can be found", shiny::a("here", href = "https://github.com/peiwenzhang123/kmeans/tree/master/App", "."),
     
     # Output graph
     mainPanel(
       tabsetPanel(type = "tabs",
         tabPanel("Barack Obama", plotOutput("ograph")),
         tabPanel("Justin Trudeau", plotOutput("tgraph"))
-   )  
+    )  
+   )
   )
  )
 )
@@ -51,7 +54,7 @@ server <- function(input, output){
     as.factor(kmeans(trudeau_kmeans[, 2:3], input$kval, nstart = 20)$cluster)
   })
   
-  # Create the graph
+  # Create the graphs
   output$ograph <- renderPlot({
     ggplot(obama_kmeans, aes(energy, valence, color = obama_cluster())) +
       labs(title = "K-Means Clustering", x = "Energy", y = "Valence", col = "Cluster") +
