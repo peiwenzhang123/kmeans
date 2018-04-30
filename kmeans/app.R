3a02cd08a70486da4de0053cc12fcf2e88cda0b3
Peiwen Zhang <peiwen.zhang1994@gmail.com>
2018-04-30 14:38:48 -0400

    fixed colors

--- kmeans/app.R
+++ kmeans/app.R
@@ -8,11 +8,11 @@ library(shiny)
 # Load data
 obama <- read_csv("obama.csv")
 obama_kmeans <- obama %>%
-  select(track_name, energy, valence)
+  select(track_name, energy, valence) 
 
 trudeau <- read_csv("trudeau.csv")
 trudeau_kmeans <- trudeau %>%
-  select(track_name, energy, valence)
+  select(track_name, energy, valence) 
 
 ### UI Side ####
 
@@ -28,7 +28,7 @@ ui <- fluidPage(
       # Allow user to choose k
       sliderInput(inputId = "kval", 
                   label = "Choose a value for k, the number of clusters: ",
-                  min = 2, max = 8, value = 3)),
+                  min = 2, max = 5, value = 3)),
     
     p("This shiny app is made for the final project of Stat041 - complete code and datasets can be found", 
       shiny::a("here", href = "https://github.com/peiwenzhang123/kmeans/tree/master/kmeans"),
@@ -46,28 +46,55 @@ ui <- fluidPage(
 
 ### Server Side ###
 server <- function(input, output){
+    
+  centers <- reactive({
+    
+    if(input$kval == 2){
+      centers <- matrix(c(rep(0.5, 2), 0.4, 0.8), nrow = 2, ncol = 2) 
+    }
+    
+    if(input$kval == 3){
+      centers <- matrix(c(0.4, rep(0.7, 2), rep(0.4, 2), 0.7), nrow = 3, ncol = 2) 
+    }
+    
+    if(input$kval == 4){
+      centers <- matrix(c(0.5, rep(0.7, 3), rep(0.4, 2), 0.6, 0.9), nrow = 4, ncol = 2) 
+    }
+    
+    if(input$kval == 5){
+      centers <- matrix(c(0.4, 0.5, 0.6, rep(0.8, 2), rep(0.4, 2), rep(0.6, 2), 0.8), nrow = 5, ncol = 2) 
+    }
+    
+    output = centers
+  })
   
   set.seed(20)
   obama_cluster <- reactive({
-    as.factor(kmeans(obama_kmeans[, 2:3], input$kval, nstart = 20)$cluster)
+    as.factor(kmeans(obama_kmeans[, 2:3], centers = centers(), nstart = 20)$cluster)
   })
+  
+  set.seed(20)
   trudeau_cluster <- reactive({
-    as.factor(kmeans(trudeau_kmeans[, 2:3], input$kval, nstart = 20)$cluster)
+    as.factor(kmeans(trudeau_kmeans[, 2:3], centers = centers(), nstart = 20)$cluster)
   })
   
   # Create the graphs
   output$ograph <- renderPlot({
     ggplot(obama_kmeans, aes(energy, valence, color = obama_cluster())) +
       labs(title = "K-Means Clustering", x = "Energy", y = "Valence", col = "Cluster") +
+      #scale_color_manual(values = pal) +
       geom_point(size = 5) +
-      theme_light()
+      theme_light() +
+      theme(legend.position="none")
   })
   
   output$tgraph <- renderPlot({
     ggplot(trudeau_kmeans, aes(energy, valence, color = trudeau_cluster())) +
       labs(title = "K-Means Clustering", x = "Energy", y = "Valence", col = "Cluster") +
+      #scale_color_manual(values = pal) +
       geom_point(size = 5) +
-      theme_light()
+      theme_light() +
+      theme(legend.position="none")
   })
 }
 
